d99375de7022197385acaef7ee145eedd3709bfa
Cloyd Balongkit <cloydbalongkit1@gmail.com>
2023-09-12 09:07:30 +0800

    Erasing the secret key and database uri Keys

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
@@ -42,7 +41,6 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
